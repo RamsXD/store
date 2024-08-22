7635a82566ba775da634c0399b6b72adc6589f96
Alessandro Ramiro <ramsxd67@gmail.com>
2024-08-21 21:33:05 -0300

    adicionei background-image, e ajustei algumas coisas no css e html

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 // cabeçalho mudando background ao usar scroll
-let header = document.querySelector.header
+let header = document.querySelector.header;
 
 function scrollAtivo() {
-    menu.classList.toggle('ativo', scrollY > 0)
+    menu.classList.toggle('ativo', scrollY > 600)
 }
 
 window.addEventListener('scroll', scrollAtivo)
